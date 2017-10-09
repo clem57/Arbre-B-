61dcc15a8b90b2d7d20ec38c20fb1dfcc97061d7
Clément <Clem@Clem-PC.home>
2017-10-09 20:11:28 +0200

    Je viens d'ajouter le projet au git

---
+++ ArbreB+/src/arbre/Noeud.java
@@ -0,0 +1,34 @@
+package arbre;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Noeud <E> {
+	public UUID iD;
+	public ArrayList<E> ensbValeurs;
+	public ArrayList<UUID> ensbPointeurs;
+	public int niveau;
+	public Noeud<E> parent;
+	public UUID voisin;
+	public float remplissage;
+	public boolean isRacine;
+	public boolean isFeuille;
+	public boolean isIntermediaire;
+		
+	public Noeud(ArrayList<E> ensbValeurs) {
+		//
+		this.iD = UUID.randomUUID();
+		this.ensbValeurs = ensbValeurs;
+		this.ensbPointeurs = null;
+		this.niveau = niveau;
+		this.parent = parent;
+		this.voisin = voisin;
+		this.remplissage = remplissage;
+		this.isRacine = isRacine;
+		this.isFeuille = isFeuille;
+		this.isIntermediaire = isIntermediaire;
+	}
+	
+	
+	
+}

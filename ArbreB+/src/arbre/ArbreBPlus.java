dd336508866995f98bc56bd92ba7cb7db5986b2e
Clément <Clem@Clem-PC.home>
2017-10-09 23:48:09 +0200

    Lundi soir

--- ArbreB+/src/arbre/ArbreBPlus.java
+++ ArbreB+/src/arbre/ArbreBPlus.java
@@ -1,5 +1,24 @@
 package arbre;
 
-public class ArbreBPlus {
+public class ArbreBPlus<E> {
+	public Noeud<E> racine;
+	public int n;
 	
+	
+	public Noeud<E> chercher(E element) {						
+		return (chercherRecursif(racine, element));					
+	}
+	
+	public Noeud<E> chercherRecursif(Noeud<E> noeud, E element){	    //rend l'arbre dont la racine est l'element recherché
+		if (noeud.isFeuille) {
+			return noeud;			
+		}
+		if (element<noeud.ensbPointeurs[0] {
+			return chercherRecursif(, element);
+		}
+		if(noeud.getRacine().compareTo(element)>0) {				//si la racine est plus grande que l'elt, on cherche sur le fils gauche
+			return chercher(noeud.getfG(), element);
+		}
+		return chercher(noeud.getfD(), element);					//si la racine est plus petite que l'elt, on cherche sur le fils droit	
+	}
 }
--- ArbreB+/src/arbre/Noeud.java
+++ ArbreB+/src/arbre/Noeud.java
@@ -3,30 +3,49 @@ package arbre;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Noeud <E> {
+public class Noeud<E> {
 	public UUID iD;
 	public ArrayList<E> ensbValeurs;
 	public ArrayList<UUID> ensbPointeurs;
-	public int niveau;
 	public Noeud<E> parent;
-	public UUID voisin;
+	public int niveau;
+	public ArrayList<UUID> voisin;
 	public float remplissage;
 	public boolean isRacine;
 	public boolean isFeuille;
 	public boolean isIntermediaire;
 		
-	public Noeud(ArrayList<E> ensbValeurs) {
+	public Noeud(ArrayList<E> ensbValeurs, boolean isRacine, boolean isFeuille, boolean isIntermediaire) {
 		this.iD = UUID.randomUUID();
 		this.ensbValeurs = ensbValeurs;
 		this.ensbPointeurs = null;
+		this.parent = determinationParent();
 		this.niveau = determinationNiveau();
-		this.parent = parent;
-		this.voisin = voisin;
-		this.remplissage = remplissage;
+		this.voisin = determinationVoisins();
+		this.remplissage = determinationRemplissage();
 		this.isRacine = isRacine;
-		this.isFeuille = isFeuille;
+		this.isFeuille = isFeuille ;
 		this.isIntermediaire = isIntermediaire;
 	}	
 	
-	public int determinationNiveau() {}
+	
+	
+	public Noeud<E> determinationParent(){
+		if (this.isRacine) {
+			return null;
+		}
+	}
+	
+	public int determinationNiveau() {
+		if (this.isRacine) {
+			return 0;
+		}
+	}	
+	
+	public ArrayList<UUID> determinationVoisins(){}
+	
+	public float determinationRemplissage() {}
+	
+	public String toString() {}
+
 }
